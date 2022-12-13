edfc95ac18307f2bb78ae36416f8cf00db03ba7c
dabuzida <dabuzida@gmail.com>
2022-12-13 11:56:33 +0900

    test

--- lib/my_checkbox.dart
+++ lib/my_checkbox.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/services.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
@@ -12,9 +13,20 @@ class MyCheckbox extends StatefulWidget {
 
 class _MyCheckboxState extends State<MyCheckbox> {
   bool isChecked = false;
+  bool isChecked2 = false;
+  bool isChecked3 = false;
+  bool isChecked4 = false;
+  bool isChecked6 = false;
+  bool isChecked7 = false;
+  bool isChecked5 = false;
+  bool isChecked8 = false;
+  bool isChecked9 = false;
+  bool isChecked10 = false;
+  bool isChecked11 = false;
   @override
   Widget build(BuildContext context) {
     Color getColor(Set<MaterialState> states) {
+      return Colors.black;
       const Set<MaterialState> interactiveStates = <MaterialState>{
         MaterialState.pressed,
         MaterialState.hovered,
@@ -29,15 +41,122 @@ class _MyCheckboxState extends State<MyCheckbox> {
     return Column(
       children: <Widget>[
         Checkbox(
+          activeColor: Colors.purple,
           checkColor: Colors.white,
-          fillColor: MaterialStateProperty.resolveWith(getColor),
+          // shape: RoundedRectangleBorder(
+          //   borderRadius: BorderRadius.all(Radius.circular(1.0)),
+          // ),
+          // fillColor: MaterialStateProperty.resolveWith(getColor),
+          // overlayColor: ,
           value: isChecked,
           onChanged: (bool? value) {
             setState(() {
               isChecked = value!;
             });
           },
-        )
+        ),
+        Checkbox(
+          side: MaterialStateBorderSide.resolveWith(
+            (states) => const BorderSide(width: 2.0, color: Colors.black),
+          ),
+          activeColor: Colors.white, // 체크시,  바탕색
+          checkColor: Colors.red, //  체크시, 체크아이콘 색
+          splashRadius: 0.0, // 버튼에 호버시, 체크박스를 감싸는 물방울 그림자
+          value: isChecked2,
+          onChanged: (bool? value) {
+            setState(() {
+              isChecked2 = value!;
+            });
+          },
+        ),
+        Checkbox(
+          side: MaterialStateBorderSide.resolveWith(
+            (states) => const BorderSide(width: 2.0, color: Colors.blue),
+          ),
+          activeColor: Colors.white, // 체크시,  바탕색
+          checkColor: Colors.blue, //  체크시, 체크아이콘 색
+          value: isChecked5,
+          onChanged: (bool? value) {
+            setState(() {
+              isChecked5 = value!;
+            });
+          },
+        ),
+        Checkbox(
+          side: MaterialStateBorderSide.resolveWith(
+            (states) => const BorderSide(width: 2.0, color: Colors.green),
+          ),
+          activeColor: Colors.white, // 체크시,  바탕색
+          checkColor: Colors.green, //  체크시, 체크아이콘 색
+          value: isChecked3,
+          onChanged: (bool? value) {
+            setState(() {
+              isChecked3 = value!;
+            });
+          },
+        ),
+        Checkbox(
+          side: MaterialStateBorderSide.resolveWith(
+            (states) => const BorderSide(width: 2.0, color: Colors.grey),
+          ),
+          activeColor: Colors.white, // 체크시,  바탕색
+          checkColor: Colors.grey, //  체크시, 체크아이콘 색
+          value: isChecked4,
+          onChanged: (bool? value) {
+            setState(() {
+              isChecked4 = value!;
+            });
+          },
+        ),
+        Container(
+          width: 110,
+          color: Colors.blue.shade100,
+          child: GestureDetector(
+            onTap: () {
+              isChecked4 = !isChecked4;
+              setState(() {});
+            },
+            child: MouseRegion(
+              cursor: SystemMouseCursors.click,
+              child: Row(
+                children: <Widget>[
+                  Checkbox(
+                    // shape: RoundedRectangleBorder(),
+                    shape: const CircleBorder(),
+                    hoverColor: null,
+                    side: MaterialStateBorderSide.resolveWith(
+                      (states) => const BorderSide(width: 2.0, color: Colors.green),
+                    ),
+                    activeColor: Colors.white, // 체크시,  바탕색
+                    checkColor: Colors.green, //  체크시, 체크아이콘 색
+                    value: isChecked4,
+                    onChanged: (bool? value) {
+                      isChecked4 = value!;
+                      setState(() {});
+                    },
+                  ),
+                  SizedBox(width: 20),
+                  Text('aaksdkl'),
+                ],
+              ),
+            ),
+          ),
+        ),
+        //
+        Checkbox(
+          side: MaterialStateBorderSide.resolveWith(
+            (states) => const BorderSide(width: 2.0, color: Colors.grey),
+          ),
+          splashRadius: 0.0,
+          activeColor: Colors.white, // 체크시,  바탕색
+          checkColor: Colors.grey, //  체크시, 체크아이콘 색
+          value: isChecked6,
+          onChanged: (bool? value) {
+            setState(() {
+              isChecked6 = value!;
+            });
+          },
+        ),
       ],
     );
   }
