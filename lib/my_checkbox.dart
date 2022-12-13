c4c1ada4e9104fb664c6153a47aaa107ab7f00af
dabuzida <dabuzida@gmail.com>
2022-12-13 09:58:23 +0900

    start

---
+++ lib/my_checkbox.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+import 'package:flutter/material.dart';
+
+class MyCheckbox extends StatefulWidget {
+  const MyCheckbox({super.key});
+
+  @override
+  State<MyCheckbox> createState() => _MyCheckboxState();
+}
+
+class _MyCheckboxState extends State<MyCheckbox> {
+  bool isChecked = false;
+  @override
+  Widget build(BuildContext context) {
+    Color getColor(Set<MaterialState> states) {
+      const Set<MaterialState> interactiveStates = <MaterialState>{
+        MaterialState.pressed,
+        MaterialState.hovered,
+        MaterialState.focused,
+      };
+      if (states.any(interactiveStates.contains)) {
+        return Colors.blue;
+      }
+      return Colors.red;
+    }
+
+    return Column(
+      children: <Widget>[
+        Checkbox(
+          checkColor: Colors.white,
+          fillColor: MaterialStateProperty.resolveWith(getColor),
+          value: isChecked,
+          onChanged: (bool? value) {
+            setState(() {
+              isChecked = value!;
+            });
+          },
+        )
+      ],
+    );
+  }
+}
